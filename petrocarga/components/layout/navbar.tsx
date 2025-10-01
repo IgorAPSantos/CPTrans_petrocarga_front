bd5b9635443d433925bce1028d769764706794c3
LuisMAAssis <Luis01MAA@hotmail.com>
2025-10-01 14:17:54 -0300

    Adicionando Next.js + Navbar + Footer

--- petrocarga/components/layout/navbar.tsx
+++ petrocarga/components/layout/navbar.tsx
@@ -1,24 +1,25 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
-import Logo from "./logo";
 
 export default function Navbar() {
     return (
         <nav className="flex items-center justify-between max-w-5xl py-6 w-full">
-            <Logo className=""/>
+            <Link className="flex items-center space-x-2" href={""}>
+                <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-gradient-to-br from-primary to-secondary">
+                    <span className="text-lg font-bold text-primary-foreground">CP</span>
+                </div>
+                <span className="text-xl font-bold text-foreground">CPTrans</span>
+            </Link>
             <ul className="flex items-center gap-6">
                 <Link href="/" className={cn(buttonVariants({ variant: "link" }))}>
-                    Home
+                    Cadastrar Vagas
                 </Link>
                 <Link href="/sobre" className={cn(buttonVariants({ variant: "link" }))}>
-                    Sobre
+                    Visualizar Vagas
                 </Link>
                 <Link href="/vagas" className={cn(buttonVariants({ variant: "link" }))}>
-                    Vagas  
-                </Link>
-                <Link href="/vagas/cadastro" className={cn(buttonVariants({ variant: "link" }))}>
-                    Cadastrar Vagas
+                    Uso das Vagas  
                 </Link>
             </ul>
         </nav>
