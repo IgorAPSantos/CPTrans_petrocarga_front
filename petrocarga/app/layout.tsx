5a89925bc85e277de92162d1c634ff488eb9f725
Jusenx <josedantas2408@gmail.com>
2025-10-08 18:45:26 -0300

    mapa corrigido

--- petrocarga/app/layout.tsx
+++ petrocarga/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <div className="flex min-h-screen flex-col">
           <Navbar />
           <main className="flex-1 relative">
-            <MapProvider>{children}</MapProvider>;
+            <MapProvider>{children}</MapProvider>
           </main>
           <Footer />
         </div>
