1491e305a5d0b0a193de42c33476807fe5260346
José Rodrigo <josedantas2408@gmail.com>
2025-11-07 04:28:02 -0300

    resolvendo problema de types ERRO reserva

--- petrocarga/lib/types/motorista.ts
+++ petrocarga/lib/types/motorista.ts
@@ -1,16 +1,21 @@
 export type Usuario = {
-    id: string;
-    nome: string;
-    email: string;
-    telefone: string;
-    cpf: string;
-}
+  id: string;
+  nome: string;
+  email: string;
+  telefone: string;
+  cpf: string;
+};
 
 export type Motorista = {
-    id: string;
-    numeroCNH: string;
-    tipoCNH: string;
-    dataValidadeCNH: string;
-    empresaId?: string | null;
-    usuario: Usuario;
-}
+  id: string;
+  nome: string;
+  cpf: string;
+  telefone: string;
+  numeroCNH: string;
+  tipoCNH: string;
+  dataValidadeCNH: string;
+  empresaId?: string | null;
+  email: string;
+  senha: string;
+  usuario: Usuario;
+};
