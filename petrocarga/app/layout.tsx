92334d3b176b5c12b938cc9278eb303ffed41013
José Rodrigo <josedantas2408@gmail.com>
2025-12-16 07:19:24 -0300

    Adicionando Icones e Manifest PWA

--- petrocarga/app/layout.tsx
+++ petrocarga/app/layout.tsx
@@ -1,31 +1,43 @@
-import { Maven_Pro } from 'next/font/google';
+import type { Metadata } from "next";
+import { Maven_Pro } from "next/font/google";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import "./globals.css";
-import { AuthProvider } from '@/context/AuthContext';
+import { AuthProvider } from "@/context/AuthContext";
 
 const mavenPro = Maven_Pro({
-    weight: "variable",
-    subsets: ["latin"],
-    variable: "--font-maven-pro"
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-maven-pro",
 });
 
-export const metadata = {
-    title: "PetroCarga",
-    description: "O Petrocarga trás aos motoristas uma plataforma eficiente para gerenciamento de cargas e rotas",
+export const metadata: Metadata = {
+  title: "PetroCarga",
+  description:
+    "O Petrocarga trás aos motoristas uma plataforma eficiente para gerenciamento de cargas e rotas",
+
+  themeColor: "#3a2bc2",
+
+  appleWebApp: {
+    capable: true,
+    title: "Petrocarga",
+    statusBarStyle: "default",
+  },
+
+  icons: {
+    apple: "/web-app-manifest-192x192.png",
+  },
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="pt-br" className={mavenPro.variable}>
-            <body>
-                <AuthProvider>
-                    {children}
-                </AuthProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="pt-br" className={mavenPro.variable}>
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
 }
---
+++ petrocarga/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Petrocarga",
+    short_name: "Petrocarga",
+    description: "Um Sistema de Reservas de Carga e Descarga em Petropolis",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#3a2bc2",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
