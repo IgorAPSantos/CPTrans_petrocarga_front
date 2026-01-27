1d746ce2a70621c756edef6ebaa3b568f190c966
José Rodrigo <josedantas2408@gmail.com>
2026-01-27 13:42:32 -0300

    feat: iplementando interatividade com notificação firebase

--- petrocarga/public/firebase-messaging-sw.js
+++ petrocarga/public/firebase-messaging-sw.js
@@ -14,19 +14,92 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  const data = payload.data || {};
+const tipoImagem = {
+  DENUNCIA: '/image-firebase/imag-denuncia.png',
+  MOTORISTA: '/image-firebase/imag-motorista.png',
+  VAGA: '/image-firebase/imag-vaga.png',
+  RESERVA: '/image-firebase/imag-reserva.png',
+  VEICULO: '/image-firebase/imag-veiculo.png',
+};
 
-  self.registration.showNotification(data.title || 'PetroCarga', {
-    body: data.body,
-    icon: '/icons/icon-192x192.png',
-    badge: '/icons/badge.png',
-    tag: data.id,
-    data: {
-      id: data.id,
-      tipo: data.tipo,
-      lida: data.lida === 'true',
-      metadata: data.metadata,
+const image = tipoImagem[data.tipo] || '';
+
+messaging.showNotification(data.title || 'PetroCarga', {
+  body: data.body || 'Você tem uma nova atualização',
+  icon: '/web-app-manifest-192x192.png',
+  badge: '/icons/badge.png',
+  image,
+
+  vibrate: [100, 50, 100],
+
+  tag: data.id || 'petrocarga',
+  renotify: true,
+
+  actions: [
+    {
+      action: 'abrir',
+      title: 'Ver detalhes',
     },
-  });
+    {
+      action: 'fechar',
+      title: 'Ignorar',
+    },
+  ],
+
+  data: {
+    url: data.url || '/',
+    id: data.id,
+    tipo: data.tipo,
+  },
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const action = event.action;
+  const data = event.notification.data;
+
+  if (action === 'fechar') {
+    return;
+  }
+
+  let url = data?.url || '/';
+
+  if (!data?.url) {
+    switch (data?.tipo) {
+      case 'DENUNCIA':
+        url = `/motorista/reservas/minhas-denuncias`;
+        break;
+      case 'MOTORISTA':
+        url = `/motoristas/reservas`;
+        break;
+      case 'VAGA':
+        url = `/motorista/reservas`;
+        break;
+      case 'RESERVA':
+        url = `/motorista/reservas`;
+        break;
+      case 'VEICULO':
+        url = `/motorista/veiculos/meus-veiculos`;
+        break;
+      default:
+        url = '/';
+    }
+  }
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: 'window',
+        includeUncontrolled: true,
+      })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url.includes(self.location.origin)) {
+            return client.focus();
+          }
+        }
+        return clients.openWindow(url);
+      }),
+  );
 });
